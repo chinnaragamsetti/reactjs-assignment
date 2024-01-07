38f93ad731bd882cbba8eb73c123613cc95f8b93
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2024-01-07 09:36:52 +0000

    src/App.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,28 @@
 import './index.css'
 
 const Listprofile = props => {
-  const {eachsearch, deletelist} = props
-  const {id, title, logoUrl, timeAccessed, domainUrl} = eachsearch
+  const {listDetails, deletelist} = props
+  const {id, title, logoUrl, timeAccessed, domainUrl} = listDetails
   const onDelete = () => {
     deletelist(id)
   }
   return (
     <li className="listcontainer">
       <div className="f">
-        <h1 className="time">{timeAccessed}</h1>
-        <img src={logoUrl} alt="img" className="listlogo" />
+        <p className="time">{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" className="listlogo" />
         <p className="title">{title}</p>
         <p className="domainurl">{domainUrl}</p>
       </div>
-      <button className="button" type="button" onClick={onDelete}>
+      <button
+        className="button"
+        type="button"
+        onClick={onDelete}
+        data-testid="delete"
+      >
         <img
           src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
-          alt="cross"
+          alt="delete"
           className="delete-button"
         />
       </button>
